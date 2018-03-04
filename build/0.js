e12218ff322fef076d5e117df8d4490773bf0f51
maddoxnelson <maddoxnelson@gmail.com>
2018-03-04 15:30:22 -0600

    14 code splitting

---
+++ build/0.js
@@ -0,0 +1,33 @@
+webpackJsonp([0],{
+
+/***/ 16:
+/*!*********************!*\
+  !*** ./app/lazy.js ***!
+  \*********************/
+/*! exports provided: default */
+/*! all exports used */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__lazy_css__ = __webpack_require__(/*! ./lazy.css */ 17);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__lazy_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__lazy_css__);
+
+
+/* harmony default export */ __webpack_exports__["default"] = ("Hello from lazy");
+
+/***/ }),
+
+/***/ 17:
+/*!**********************!*\
+  !*** ./app/lazy.css ***!
+  \**********************/
+/*! dynamic exports provided */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
