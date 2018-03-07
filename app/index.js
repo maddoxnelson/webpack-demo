5d91f023e82e62898aebe00906b26d5cdbd13f2a
maddoxnelson <maddoxnelson@gmail.com>
2018-03-07 12:13:38 -0600

    17 tree shaking

--- app/index.js
+++ app/index.js
@@ -3,5 +3,8 @@ import component from "./component";
 import "purecss"
 import "font-awesome/css/font-awesome.css";
 import "react";
+import { bake } from "./shake";
 
 document.body.appendChild(component());
+
+bake();
